0c71b9abc588fce01727724171aaf1656819aedd
Gaurav <gaurav.bhe.24@gmail.com>
2024-04-12 21:41:52 +0530

    Signup logic with password encrypted done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig.js');
+const apiRoutes = require('./routes/index.js')
 
-
+const app = express();
 
 const prepareAndStartServer = () => {
 
-    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
     
     app.listen(PORT, () => {
         console.log(`Server is running ${PORT}`);
@@ -15,5 +20,6 @@ const prepareAndStartServer = () => {
 }
 
 prepareAndStartServer();
+
 
 
