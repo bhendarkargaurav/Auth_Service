e879f07715e4523d2aa418543329213053063506
Gaurav <gaurav.bhe.24@gmail.com>
2024-04-14 18:20:12 +0530

    Added email and password validation for incomming request

--- src/middlewares/auth-request-validators.js
+++ src/middlewares/auth-request-validators.js
@@ -1,3 +1,5 @@
+// code to handele client mistakes
+
 const validateUserAuth = (req, res, next) => {
     if(!req.body.email || !req.body.password) {
         return res.status(400).json({
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -40,6 +40,23 @@ class UserService {
         }
     }
 
+    async isAuthenticated(token) {
+        try {
+            const response = this.verifyToken(token);
+            if(!response) {
+                throw {error: 'invalid Token'}
+            }
+            const user = this.UserRepository.getById(response.id);
+            if(!user) {
+                throw {error: 'No user with the corresponding token exist'}
+            }
+            return user.id;
+        } catch (error) {
+            console.log("something went wrong in the service layer");
+            throw error;  
+        }
+    }
+
     createToken(user) {
         try {
             const result = jwt.sign(user, JWT_KEY, {expiresIn: "1d"});
