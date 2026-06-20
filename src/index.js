d58476b80d1dd43f4bc4195d59162f17ce5e0612
Gaurav <gaurav.bhe.24@gmail.com>
2026-06-20 23:04:35 +0530

    project setup update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {config} = require("./config/config.json")
+
 const { PORT } = require('./config/serverConfig.js');
 const apiRoutes = require('./routes/index.js');
 
 const db = require('./models/index.js');
 
 const app = express();
+   
 
-const prepareAndStartServer = () => {
+
+const prepareAndStartServer = async() => {
+
+    app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.use('/api', apiRoutes);
+
+//     const env = process.env.NODE_ENV || 'development';
+// const config = require('./config/config.json')[env];
+// console.log("ENV =", env);
+// console.log(config);
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
@@ -21,6 +33,13 @@ const prepareAndStartServer = () => {
             db.sequelize.sync({alter: true});
         }
     });
+
+     try {
+        await db.sequelize.sync({ alter: true });
+        console.log("Database synced successfully");
+    } catch (error) {
+        console.log("Sync Error:", error);
+    }
 }   
 
 prepareAndStartServer();
